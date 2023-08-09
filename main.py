30a354df55711875c11f1134caa5699123c8d594
ABDESSAMADE OUOMAR <119781541+ouomarabdessamade@users.noreply.github.com>
2023-08-10 00:26:24 +0100

    main.py

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 from utility.helper import *
 from utility.batch_test import *
 
+
 if __name__ == '__main__':
     os.environ["CUDA_VISIBLE_DEVICES"] = str(args.gpu_id)
 
@@ -49,21 +50,7 @@ if __name__ == '__main__':
     else:
         pretrain_data = None
 
-    # Set the GPU device
-    gpu_id = 0  # Replace '0' with your desired GPU device ID
-    device_str = f'/gpu:{gpu_id}'
-
-    # Allow GPU memory growth
-    config_proto = tf.compat.v1.ConfigProto()
-    config_proto.gpu_options.allow_growth = True
-    sess = tf.compat.v1.Session(config=config_proto)
-
-    # Set the default session for TensorFlow
-    tf.compat.v1.keras.backend.set_session(sess)
-
-    # Create your model and move it to the GPU
-    with tf.device(device_str):
-        model = GCMC(data_config=config, pretrain_data=pretrain_data)
+    model = LightGCN(data_config=config, pretrain_data=pretrain_data)
 
     """
     *********************************************************
@@ -78,6 +65,10 @@ if __name__ == '__main__':
         ensureDir(weights_save_path)
         save_saver = tf.train.Saver(max_to_keep=1)
 
+    config = tf.ConfigProto()
+    config.gpu_options.allow_growth = True
+    sess = tf.Session(config=config)
+
     """
     *********************************************************
     Reload the pretrained model parameters.
@@ -88,6 +79,7 @@ if __name__ == '__main__':
         pretrain_path = '%sweights/%s/%s/%s/l%s_r%s' % (args.weights_path, args.dataset, model.model_type, layer,
                                                         str(args.lr), '-'.join([str(r) for r in eval(args.regs)]))
 
+
         ckpt = tf.train.get_checkpoint_state(os.path.dirname(pretrain_path + '/checkpoint'))
         if ckpt and ckpt.model_checkpoint_path:
             sess.run(tf.global_variables_initializer())
@@ -95,18 +87,16 @@ if __name__ == '__main__':
             print('load the pretrained model parameters from: ', pretrain_path)
 
             # *********************************************************
-            # Get the performance from pretrained model.
+            # get the performance from pretrained model.
             if args.report != 1:
                 users_to_test = list(data_generator.test_set.keys())
                 ret = test(sess, model, users_to_test, is_valid=False, drop_flag=True)
                 cur_best_pre_0 = ret['recall'][0]
 
-                pretrain_ret = 'pretrained model recall=[%.5f], precision=[%.5f], hit=[%.5f],' \
-                               'ndcg=[%.5f]' % \
-                               (ret['recall'][0],
-                                ret['precision'][0],
-                                ret['hit_ratio'][0],
-                                ret['ndcg'][0])
+                pretrain_ret = 'pretrained model recall=[%.5f], precision=[%.5f], ndcg=[%.5f]' % \
+                               ('\t'.join(['%.5f' % r for r in ret['recall']]),
+                                '\t'.join(['%.5f' % r for r in ret['precision']]),
+                                '\t'.join(['%.5f' % r for r in ret['ndcg']]))
                 print(pretrain_ret)
         else:
             sess.run(tf.global_variables_initializer())
@@ -138,12 +128,11 @@ if __name__ == '__main__':
         for i, users_to_test in enumerate(users_to_test_list):
             ret = test(sess, model, users_to_test, is_valid=False, drop_flag=True)
 
-            perf_str = 'Epoch %d [%.1fs + %.1fs]: train==[%.5f], recall=[%.5f], ' \
-                       'precision=[%.5f], hit=[%.5f], ndcg=[%.5f]' % \
-                       (epoch, t2 - t1, t3 - t2, loss, ret['recall'][0],
-                        ret['precision'][0], ret['hit_ratio'][0],
-                        ret['ndcg'][0])
-            print(perf_str)
+            final_perf = "recall=[%s], precision=[%s], ndcg=[%s]" % \
+                         ('\t'.join(['%.5f' % r for r in ret['recall']]),
+                          '\t'.join(['%.5f' % r for r in ret['precision']]),
+                          '\t'.join(['%.5f' % r for r in ret['ndcg']]))
+            print(final_perf)
 
             f.write('\t%s\n\t%s\n' % (split_state[i], final_perf))
         f.close()
@@ -153,7 +142,7 @@ if __name__ == '__main__':
     *********************************************************
     Train.
     """
-    loss_loger, pre_loger, rec_loger, ndcg_loger, hit_loger = [], [], [], [], []
+    loss_loger, pre_loger, rec_loger, ndcg_loger = [], [], [], [], []
     stopping_step = 0
     should_stop = False
 
@@ -170,51 +159,60 @@ if __name__ == '__main__':
                                           model.mess_dropout: eval(args.mess_dropout),
                                           model.neg_items: neg_items})
             loss += batch_loss
-            mf_loss += batch_mf_loss
-            emb_loss += batch_emb_loss
-            reg_loss += batch_reg_loss
+            #mf_loss += batch_mf_loss
+            #emb_loss += batch_emb_loss
+            #reg_loss += batch_reg_loss
 
         if np.isnan(loss) == True:
             print('ERROR: loss is nan.')
             sys.exit()
 
-        # Print the validation evaluation metrics each 10 epochs; pos:neg = 1:10.
-        if (epoch + 1) % 10 != 0:
+        # print the validation evaluation metrics each 10 epochs; pos:neg = 1:10.
+        if (epoch + 1) % 1 != 0:
             if args.verbose > 0 and epoch % args.verbose == 0:
                 perf_str = 'Epoch %d [%.1fs]: train==[%.5f]' % (
                     epoch, time() - t1, loss)
                 print(perf_str)
             continue
 
-        
-        #-----------------------Test---------------
         t2 = time()
-        users_to_test = list(data_generator.test_set.keys())
-        ret = test(sess, model, users_to_test, is_valid=False, drop_flag=True)
+        users_to_valid = list(data_generator.valid_set.keys())
+        ret = test(sess, model, users_to_valid, is_valid=True, drop_flag=True)
+
         t3 = time()
 
         loss_loger.append(loss)
         rec_loger.append(ret['recall'])
         pre_loger.append(ret['precision'])
         ndcg_loger.append(ret['ndcg'])
-        hit_loger.append(ret['hit_ratio'])
-          
+
+        '''
+            save model with the best recall@20 under validation set
+        '''
+        # if cur_best_pre_0 < ret['recall'][0]:
+        #     save_saver.save(sess, weights_save_path + '/weights', global_step=epoch)
+        #     print('save the weights in path: ', weights_save_path)
+        #     # tf.saved_model.save(model, model_folder)
+            
         if args.verbose > 0:
-            perf_str = f'Test -- Epoch {epoch} [%.1fs]: train==[%.5f], recall=[%.5f], ' \
-                        f'precision=[%.5f], hit=[%.5f], ndcg=[%.5f]' % \
-                        (t3 - t2, loss.item(), ret['recall'][0], 
-                          ret['precision'][0], ret['hit_ratio'][0],
-                          ret['ndcg'][0])
+            perf_str = 'Validation -- Epoch %d [%.1fs + %.1fs]: train==[%.5f], recall=[%.5f], ' \
+                       'precision=[%.5f], ndcg=[%.5f]' % \
+                       (epoch, t2 - t1, t3 - t2, loss,
+                        '\t'.join(['%.5f' % r for r in ret['recall']]),
+                         '\t'.join(['%.5f' % r for r in ret['precision']]),
+                         '\t'.join(['%.5f' % r for r in ret['ndcg']]))
             print(perf_str)
 
         cur_best_pre_0, stopping_step, should_stop = early_stopping(ret['recall'][0], cur_best_pre_0,
                                                                     stopping_step, expected_order='acc', flag_step=50)
 
-        # Early stopping when cur_best_pre_0 is decreasing for ten successive steps.
+        # *********************************************************
+        # early stopping when cur_best_pre_0 is decreasing for ten successive steps.
         if should_stop == True:
             break
 
-        # Save the user & item embeddings for pretraining.
+        # *********************************************************
+        # save the user & item embeddings for pretraining.
         if ret['recall'][0] == cur_best_pre_0 and args.save_flag == 1:
             save_saver.save(sess, weights_save_path + '/weights', global_step=epoch)
             print('save the weights in path: ', weights_save_path)
@@ -226,23 +224,26 @@ if __name__ == '__main__':
     pretrain_path = '%sweights/%s/%s/%s/l%s_r%s' % (args.weights_path, args.dataset, model.model_type, layer,
                                                     str(args.lr), '-'.join([str(r) for r in eval(args.regs)]))
 
+    # weights_save_path = '%sweights/%s/%s/%s/l%s_r%s' % (args.weights_path, args.dataset, model.model_type, layer,
+    #                                                         str(args.lr), '-'.join([str(r) for r in eval(args.regs)]))
+
     ckpt = tf.train.get_checkpoint_state(os.path.dirname(pretrain_path + '/checkpoint'))
     if ckpt and ckpt.model_checkpoint_path:
         sess.run(tf.global_variables_initializer())
         saver.restore(sess, ckpt.model_checkpoint_path)
         print('load the best validation model parameters from: ', pretrain_path)
 
-        # Get the performance from pretrained model.
+        # *********************************************************
+        # get the performance from pretrained model.
+      
         users_to_test = list(set(data_generator.train_items.keys()).intersection(set(data_generator.test_set.keys())))
         ret = test(sess, model, users_to_test, is_valid=False, drop_flag=True)
         # cur_best_pre_0 = ret['recall'][0]
 
-        final_perf = '******* Best validation model recall=[%.5f], precision=[%.5f], hit=[%.5f],' \
-                        'ndcg=[%.5f]' % \
-                        (ret['recall'][0],
-                        ret['precision'][0],
-                        ret['hit_ratio'][0],
-                        ret['ndcg'][0])
+        final_perf = '******* Best validation model recall=[%.5f], precision=[%.5f], ndcg=[%.5f]' % \
+                        ('\t'.join(['%.5f' % r for r in ret['recall']]),
+                         '\t'.join(['%.5f' % r for r in ret['precision']]),
+                         '\t'.join(['%.5f' % r for r in ret['ndcg']]))
         print(final_perf)
 
     save_path = '%soutput/%s/%s.result' % (args.proj_path, args.dataset, model.model_type)
